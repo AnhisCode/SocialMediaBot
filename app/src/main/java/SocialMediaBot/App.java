e4b3d68d14eb9e65cca71339ece5e42b24522b9c
Anh <daoanh440@gmail.com>
2021-12-28 19:09:42 +1100

    test commit

--- app/src/main/java/SocialMediaBot/App.java
+++ app/src/main/java/SocialMediaBot/App.java
@@ -4,11 +4,8 @@
 package SocialMediaBot;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Hello World!");
     }
 }
